19cc77ae138b7e8533405def06b661ef640c203b
Elizabeth Munoz <munoz_714@live.com>
2020-04-24 19:53:31 -0700

    working delete route

--- server.js
+++ server.js
@@ -5,12 +5,32 @@ var dbJson = require("./db/db.json")
 var app = express();
 var PORT = 3000;
 
+app.use(express.static('public'))
+var bodyParser = require('body-parser')
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
+
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "public", "notes.html"));
 });
 app.get("/api/notes", function (req, res) {
     res.json(dbJson);
 });
+app.delete("/api/notes/:id", function (req, res) {
+    // console.log(' this is the id', req.params.id)
+    let keepNote = []
+    dbJson.forEach(function (singleNote) {
+        console.log(singleNote);
+        if (singleNote.id != req.params.id) {
+            keepNote.push(singleNote);
+        }
+    });
+    dbJson = keepNote;
+    res.json
+});
+
 app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
